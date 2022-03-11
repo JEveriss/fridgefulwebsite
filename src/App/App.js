8fecbb4f2410128a2da73df60e33a1664d044e2e
Jacques Everiss <jacques.everiss@gmail.com>
2022-03-11 13:52:08 +0000

    forgot to add home

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import "./App.scss";
 import About from "../pages/about/about";
 import Contact from "../pages/contact/contact";
-// import Home from "../pages/home/home";
+import Home from "../pages/home/home";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      {/* <Home /> */}
+      <Home />
       <About />
       <Contact />
       <Footer />
