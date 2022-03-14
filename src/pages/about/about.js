2c80c7d0a8d42de851259d21cc3d0d3d59dfff4e
Jacques Everiss <jacques.everiss@gmail.com>
2022-03-14 11:14:06 +0000

    outline added

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./about.scss";
 import qrcode from "../../assets/qrcode.svg";
 import AppInUse from "../../components/appInUse/appInUse";
+import Outline from "../contact/outline";
 
 export default function About() {
   return (
@@ -12,13 +13,14 @@ export default function About() {
           <img className="qrcode" src={qrcode} alt="QR Code"></img>
           <p>Scan the code to get the app</p>
           <p>and start cooking Fridgefuly today</p>
-          </div>
-          <h2>
-            Fridgeful was designed to help you reduce food waste by suggesting
-            new delicious recipes based on the contents of your fridge.
-          </h2>
         </div>
-      <AppInUse />
+        <h2>
+          Fridgeful was designed to help you reduce food waste by suggesting new
+          delicious recipes based on the contents of your fridge.
+        </h2>
       </div>
+      <AppInUse />
+      <Outline />
+    </div>
   );
 }
---
+++ src/pages/contact/outline.js
@@ -0,0 +1,11 @@
+import outline from "../../assets/outline.svg";
+
+import React from "react";
+
+export default function Outline() {
+  return (
+    <div>
+      <img src={outline} alt="outline" className="outline" />{" "}
+    </div>
+  );
+}
